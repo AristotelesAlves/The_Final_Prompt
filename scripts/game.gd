58cf7ab9f50b98d0b611e186b369ae7c321fafaf
Aristoteles Alves <arystotelys@gmail.com>
2026-05-24 22:10:42 -0300

    feature: adiciona cenário com tilemap e ataque do jogador

---
+++ scripts/game.gd
@@ -0,0 +1,16 @@
+extends Node2D
+@onready var player: CharacterBody2D = $player
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player.position.y > 800:
+		player.position.y = 400
+		player.position.x = 10
+	print(player.position.x)
+	print(player.position.y)
+	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,34 +4,52 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
+const ATTACK_TIME = 0.35
+
+var is_attacking := false
+var facing_right := true
 
 func _physics_process(delta: float) -> void:
-	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Movement
 	var direction := Input.get_axis("left", "right")
 
+	# Movimento
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# Animation
+	# Guarda direção do personagem
 	if direction > 0:
-		animetion.flip_h = false
-		animetion.play("walk")
-
+		facing_right = true
 	elif direction < 0:
-		animetion.flip_h = true
-		animetion.play("walk")
+		facing_right = false
 
-	else:
-		animetion.play("idle")
+	# Aplica flip UMA VEZ
+	animetion.flip_h = not facing_right
+
+	# Ataque
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		start_attack()
+
+	# Animações
+	if not is_attacking:
+		if direction != 0:
+			animetion.play("walk")
+		else:
+			animetion.play("idle")
 
 	move_and_slide()
+
+func start_attack() -> void:
+	is_attacking = true
+	animetion.play("attack")
+
+	await get_tree().create_timer(ATTACK_TIME).timeout
+
+	is_attacking = false
